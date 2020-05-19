6f097279d227884366f3a39e4937312ee2b3325e
dnlsgv <segoviadaniel2@gmail.com>
2020-05-19 16:45:18 -0400

    init

---
+++ YWxjby90ZXN0LWRhbmllbA==.js
@@ -0,0 +1,114 @@
+(function (a, b) {
+    a[b] = a[b] || {};
+    a[b].active = true;
+    a[b].elements = {};
+    a[b].elements.modal = document.createElement("div");
+    a[b].elements.modal.style.cssText =
+        "visibility: hidden;" +
+        "position: fixed;" +
+        "z-index: 2147483647;" +
+        "left: 0;" +
+        "top: 0;" +
+        "width: 100%;" +
+        "height: 100%;" +
+        "overflow: hidden;" +
+        "background-color: rgba(0, 0, 0, 0.6);" +
+        "display: -webkit-box;" +
+        "display: -ms-flexbox;" +
+        "display: flex;" +
+        "-webkit-box-pack: center;" +
+        "-ms-flex-pack: center;" +
+        "justify-content: center;" +
+        "-webkit-box-align: center;" +
+        "-ms-flex-align: center;" +
+        "align-items: center";
+    a[b].elements.content = document.createElement("div");
+    a[b].elements.content.style.cssText = "overflow:hidden;" + "background-color: #fefefe;" + "margin: 15% auto;" + "padding: 0px;" + "border: 1px solid #888;" + "width: 600px;" + "border-radius: 6px";
+    a[b].elements.close = document.createElement("span");
+    a[b].elements.close.style.cssText = "color: #000;" + "float: right;" + "font-size: 28px;" + "font-weight: bold;" + "display: none";
+    a[b].elements.close.innerHTML = "&times;";
+    a[b].elements.close.onclick = function () {
+        a[b].elements.modal.style.visibility = "hidden";
+    };
+    a[b].elements.iframe = document.createElement("iframe");
+    a[b].elements.iframe.src = './YWxjby90ZXN0LWRhbmllbA==.html';
+    a[b].elements.iframe.style.cssText = "overflow:hidden";
+    a[b].elements.iframe.frameBorder = "0";
+    a[b].elements.iframe.width = "100%";
+    a[b].elements.iframe.scrolling = "no";
+    a[b].elements.iframe.onload = function (c) {
+        a[b].elements.iframe.height = a[b].elements.content.offsetHeight + "px";
+        a[b].elements.iframe.contentWindow.postMessage("addEventListenerToCloseButton", "*");
+        a[b].elements.iframe.contentWindow.postMessage("getFullIframeHeight", "*");
+    };
+
+    window.onresize = function(event) {
+        var realBrowserWidth;
+        if(document.body && document.body.offsetWidth) {
+            realBrowserWidth = document.body.offsetWidth;
+        }
+        if(document.compatMode=='CSS1Compat' && document.documentElement && document.documentElement.offsetWidth ) {
+            realBrowserWidth = document.documentElement.offsetWidth;
+        }
+        if(window.innerWidth) {
+            realBrowserWidth = window.innerWidth;
+        }
+
+        console.log('Fuera del iframe, width: ' + realBrowserWidth)
+        // console.log(realBrowserWidth);
+
+        // if(realBrowserWidth < 576 ) {
+        //     console.log('XS');
+        // }
+
+        // if(realBrowserWidth >= 577 && realBrowserWidth < 1200) {
+        //     console.log('MD');
+        // }
+
+        // if(realBrowserWidth >= 1200) {
+        //     console.log('LG');
+        // }
+    }
+
+    a[b].functions = {};
+    a[b].functions.closeSurvey = function () {
+        a[b].elements.modal.style.visibility = "hidden";
+    };
+    a[b].functions.openSurvey = function () {
+        if (a[b].active) a[b].elements.modal.style.visibility = "visible";
+    };
+    a[b].elements.content.appendChild(a[b].elements.close);
+    a[b].elements.content.appendChild(a[b].elements.iframe);
+    a[b].elements.modal.appendChild(a[b].elements.content);
+    document.body.appendChild(a[b].elements.modal);
+    a.onmessage = function (c) {
+        if (c.data === "apolloCloseSurvey-YWxjby90ZXN0LWRhbmllbA==") {
+            a[b].active = false;
+            a[b].functions.closeSurvey();
+        }
+        if (c.data.eventId === "resizeModalHeight-YWxjby90ZXN0LWRhbmllbA==") {
+            var d = c.data.data.realIframeFullHeight;
+            a[b].elements.content.style.height = d + "px";
+            a[b].elements.iframe.style.height = d + "px";
+        }
+        if (c.data.eventId === "showCloseModalButton-YWxjby90ZXN0LWRhbmllbA==") {
+            a[b].elements.close.style = "float: right;" + "font-weight: lighter;" + "cursor: pointer;" + "font-size: 40px;" + "margin-right: 15px;" + "color: #4A4E52;";
+            a[b].elements.close.style.display = "block";
+        }
+    };
+    document.addEventListener(
+        "openApolloModal",
+        function (c) {
+            var d = 100 - 100;
+            var e = (function (a, b) {
+                return Math.random() * (b - a) + a;
+            })(1, 100);
+            if (a[b].active)
+                if (e >= d)
+                    setTimeout(function () {
+                        a[b].elements.modal.style.visibility = "visible";
+                    }, 2);
+        },
+        true
+    );
+})(window, "apollo-YWxjby90ZXN0LWRhbmllbA==");
---
+++ iframe-survey-script.js
@@ -0,0 +1,186 @@
+
+    (function(_window) {
+        function getCookie(name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie !== '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = cookies[i].trim();
+                    if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                        cookieValue = decodeURIComponent(
+                            cookie.substring(name.length + 1)
+                        );
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+
+        var csrftoken = getCookie('csrftoken');
+        _window._apollo_frame = {};
+        _apollo_frame = {};
+
+        var json = {"pages": [{"name": "p\u00e1gina1", "title": "hola **daniel**", "elements": [{"name": "b_nps", "type": "rating", "title": "[NPS rating] Ingresa el texto **requerido** haciendo click aqu\u00ed", "rateMax": 10, "rateMin": 0, "isRequired": true, "requiredErrorText": {"es": "Debe ingresar una calificaci\u00f3n"}}, {"name": "b_verbatim-1", "type": "comment", "title": "[NPS entre 0 y 6] Escribe el texto para cuando b_nps sea menor a 6", "visibleIf": "{b_nps} <= 6", "isRequired": true, "placeHolder": {"es": "Comente"}, "requiredErrorText": {"es": "Debe ingresar una respuesta"}}, {"name": "b_verbatim-2", "type": "comment", "title": "[NPS entre 7 y 8] Escribe el texto para cuando b_nps est\u00e9 entre 7 y 8", "visibleIf": "{b_nps} = 7 or {b_nps} = 8", "isRequired": true, "placeHolder": {"es": "Comente"}, "requiredErrorText": {"es": "Debe ingresar una respuesta"}}, {"name": "b_verbatim-3", "type": "comment", "title": "[NPS entre 9 y 10] Escribe el texto para cuando b_nps est\u00e9 entre 9 y 10", "visibleIf": "{b_nps} >= 9", "isRequired": true, "placeHolder": {"es": "Comente"}, "requiredErrorText": {"es": "Debe ingresar una respuesta"}}, {"html": "[Texto inferior] este es un _texto_ de **ejemplo**", "name": "pregunta1", "type": "html", "visible": false}], "description": "texto _cursiva_ y este en **negrita**"}], "mainSurveyColor": "#11CEEB", "sideButtonColor": "#99FF00", "buttonTextNotNow": "Ahora no", "surveyFontFamily": "PFBeauSansPro", "surveyTitleColor": "#F2C110", "surveyQuestionColor": "#1B0303", "surveyDisclaimerColor": "#040000"};
+
+        json = {"pages": [{"name": "página1", "title": "¡Cuéntanos comosafasfas te fue!", "elements": [{"name": "b_nps", "type": "rating", "title": "¿Qué tanto recomendarías Banco Falabella a un familiar o amigo?", "rateMax": 10, "rateMin": 0, "isRequired": true, "requiredErrorText": {"es": "Debe ingresar una calificación"}, "maxRateDescription": "Lo recomendaría", "minRateDescription": "No lo recomendaría"}, {"name": "b_verbatim-1", "type": "comment", "title": "¿Qué es lo que valoras de Banco Falabella? I", "visibleIf": "{b_nps} <= 6", "isRequired": true, "placeHolder": {"es": "Comente", "default": "Tu opinión es muy importante para nosotros."}, "requiredErrorText": {"es": "Debe ingresar una respuesta"}}, {"name": "b_verbatim-2", "type": "comment", "title": "¿Qué es lo que valoras de Banco Falabella? II", "visibleIf": "{b_nps} = 7 or {b_nps} = 8", "isRequired": true, "placeHolder": {"es": "Comente"}, "requiredErrorText": {"es": "Debe ingresar una respuesta"}}, {"name": "b_verbatim-3", "type": "comment", "title": "¿Qué es lo que valoras de Banco Falabella? III", "visibleIf": "{b_nps} >= 9", "isRequired": true, "placeHolder": {"es": "Comente"}, "requiredErrorText": {"es": "Debe ingresar una respuesta"}}, {"html": "[Texto inferior] este es un _texto_ de **ejemplo**", "name": "pregunta1", "type": "html", "visible": false}], "description": "Basado en tu experiencia. "}], "completeText": "Enviar", "completedHtml": "<h1>¡Gracias por ayudarnos a ser mejores!</h1>\n<p>Estamos constantemente trabajando para mejorar tu experiencia y tus\ncomentarios siempre son de gran ayuda. ¡Gracias por preferirnos!</p>", "mainSurveyColor": "#0BE0E7", "buttonTextNotNow": "Ahora no", "surveyFontFamily": "PFBeauSansPro", "surveyTitleColor": "#34951E", "showQuestionNumbers": "off"};
+        var extras = {"b_algo": "HOLA"};
+        var defaultThemeColors = {};
+
+        defaultThemeColors["$main-color"] = json.mainSurveyColor;
+        defaultThemeColors["$main-hover-color"] = json.mainSurveyColor;
+        defaultThemeColors["$header-color"] = json.mainSurveyColor;
+        defaultThemeColors["$text-color"] = (json.surveyQuestionColor === undefined ? 'black' : json.surveyQuestionColor);
+        Survey.StylesManager.ThemeColors["new_theme"] = defaultThemeColors;
+        Survey.StylesManager.applyTheme("new_theme");
+
+        _apollo_frame.script = new Survey.Model(json);
+
+        _apollo_frame
+            .script
+            .onComplete
+            .add(function(result) {
+                if(extras === null) {
+                    extras = {};
+                }
+                axios({
+                    method: 'post',
+                    headers: {
+                        'X-CSRFToken': csrftoken
+                    },
+                    url: '/save_survey/YWxjby90ZXN0LWRhbmllbA==',
+                    data: {
+                        answer: result.data,
+                        extras: extras
+                    }
+                })
+                .then(function (response) {
+                })
+                .catch(function (error) {
+                });
+            });
+
+        var converter = new showdown.Converter();
+        _apollo_frame.script.onTextMarkdown.add(function (survey, options) {
+                var str = converter.makeHtml(options.text);
+                str = str.substring(3);
+                str = str.substring(0, str.length - 4);
+                options.html = str;
+            });
+
+        var notNowButtonAdded = false;
+        _apollo_frame.script.onAfterRenderQuestion.add(function() {
+            if(notNowButtonAdded) return;
+            var notNowButton = document.createElement("button");
+
+            notNowButton.id = 'close-modal-survey-button';
+            notNowButton.type = "button";
+            notNowButton.className = "btn btn-info btn-xs close-modal not-now-button";
+            notNowButton.innerHTML = (json.buttonTextNotNow || 'Ahora no');
+
+            document.querySelector('.sv_nav').appendChild(notNowButton);
+
+            notNowButtonAdded = true;
+            var surveyTitleElement = document.getElementsByClassName('sv_page_title')[0];
+            if(surveyTitleElement !== undefined) {
+                surveyTitleElement.style.color = json.surveyTitleColor;
+            }
+
+            var surveyBody = document.getElementsByClassName('sv_main');
+            if(surveyBody.length > 0) {
+                surveyBody[0].style.fontFamily = json.surveyFontFamily;
+            }
+
+            //Cambiar de posición el "minRateDescription"
+            console.log("json.elements.minRateDescription: ");
+            var minRateDescription = json.pages[0].elements[0].minRateDescription;
+            if(minRateDescription !== null && minRateDescription !== undefined) {
+                //document.getElementsByClassName('sv_q_rating_max_text').style.display = 'none';
+            }
+            
+        });
+
+        var newModalFooter = json.pages[0].elements[json.pages[0].elements.length - 1].html;
+        if(newModalFooter !== undefined) {
+            newModalFooter = converter.makeHtml(newModalFooter);
+            var disclaimerElement = document.createElement('div');
+
+            disclaimerElement.id = 'survey-footer';
+            disclaimerElement.innerHTML = '<div class="modal-footer-dynamic-content" ' + 'style="color: ' + json.surveyDisclaimerColor + "; font-family: " + json.surveyFontFamily + ';">' + newModalFooter + '</div>';
+            document.getElementsByTagName('body')[0].appendChild(disclaimerElement);
+        }
+
+        _apollo_frame.element = new Vue({
+            el: '#surveyElement',
+            data: {
+                survey: _apollo_frame.script
+            }
+        });
+
+        const MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+        const targetSurveyDOM = document.getElementById('surveyElement');
+        const config = {
+            attributes: true,
+            childList: true,
+            characterData: true,
+            subtree: true,
+        };
+
+        const observer = new MutationObserver(function(mutations) {
+            parent.window.postMessage({
+                eventId: 'resizeModalHeight-YWxjby90ZXN0LWRhbmllbA==',
+                data: {
+                    realIframeFullHeight: getRealIframeFullHeight(0, 0)
+                }
+            }, '*');
+
+            let elementWithFinalMessage = document.getElementsByClassName('sv_completed_page');
+            if(elementWithFinalMessage.length > 0) {
+                parent.window.postMessage({
+                    eventId: 'resizeModalHeight-YWxjby90ZXN0LWRhbmllbA==',
+                    data: {
+                        realIframeFullHeight: getRealIframeFullHeight(46 * 2, 0)
+                    }
+                }, '*');
+                observer.disconnect();                
+            }
+
+        });
+
+        observer.observe(targetSurveyDOM, config);
+
+        function getRealIframeFullHeight(increment, decrement) {
+            var disclaimerElementHeight = (document.getElementById('survey-footer') === null ? 0 : document.getElementById('survey-footer').offsetHeight);
+            var realIframeFullHeight = document.body.children[0].offsetHeight + disclaimerElementHeight;
+
+            return realIframeFullHeight + increment - decrement;
+        }
+
+        window._apollo_frame.script.onComplete.add(function (result) {
+            let surveyFooter = document.getElementById('survey-footer');
+            if(surveyFooter !== null) {
+                surveyFooter.style.display = 'none';
+            }
+            parent.window.postMessage({
+                eventId: 'showCloseModalButton-YWxjby90ZXN0LWRhbmllbA=='
+            }, '*');
+        });
+
+        window.onmessage = function(e){
+            if(e.data == 'addEventListenerToCloseButton') {
+                document.getElementById('close-modal-survey-button').addEventListener('click', function() {
+                    parent.window.postMessage('apolloCloseSurvey-YWxjby90ZXN0LWRhbmllbA==', '*');
+                });
+            }
+
+            if(e.data == 'getFullIframeHeight') {
+                parent.window.postMessage({
+                    eventId: 'resizeModalHeight-YWxjby90ZXN0LWRhbmllbA==',
+                    data: {
+                        realIframeFullHeight: getRealIframeFullHeight(0, 0)
+                    }
+                }, '*');
+            }
+        };
+
+    })(window);
